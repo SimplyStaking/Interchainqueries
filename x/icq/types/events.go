46cb9a4a450c254b143672bcf0c738c5a4517df6
Yolozhinov <vitalyvolozhinov@gmail.com>
2022-05-25 12:26:14 +0200

    Removing events

--- x/icq/types/events.go
+++ x/icq/types/events.go
@@ -1,15 +1,1 @@
 package types
-
-const (
-	AttributeKeyQueryId         = "query_id"
-	AttributeKeyPath            = "path"
-	AttributeKeyClientId        = "client_id"
-	AttributeKeyTargetHeight    = "target_height"
-	AttributeKeyTimeoutHeight   = "timeout_height"
-	AttributeKeyChainId         = "chain_id"
-	AttributeKeyQueryParameters = "query_parameters"
-	AttributeKeyPeriodicId      = "periodic_id"
-
-	AttributeValueCategory = ModuleName
-	AttributeValueQuery    = "query"
-)
